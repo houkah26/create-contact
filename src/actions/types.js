b46f84343d5efbec98d3034e6e659f5a3494ba7b
houkah26 <houkah26@gmail.com>
2017-11-08 14:27:09 -0700

    rename action type

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,4 +2,4 @@
 export const CREATE_CONTACT_REQUEST = "create_contact_request",
   CREATE_CONTACT_SUCCESS = "create_contact_success",
   CREATE_CONTACT_FAILURE = "create_contact_failure",
-  CLEAR_CREATE_CONTACT = "clear_quote";
+  CLEAR_CREATE_CONTACT = "clear_create_contact";
